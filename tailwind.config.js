d11175e5dc08793fd918640a6e217968499026f2
Siddesh-Pansare93 <siddeshpansare207@gmail.com>
2025-02-05 23:34:21 +0530

    Add Tailwind CSS configuration file

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./App.js",
+    "./app/**/*.js",
+    "./node_modules/nativewind/**/*.js",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
